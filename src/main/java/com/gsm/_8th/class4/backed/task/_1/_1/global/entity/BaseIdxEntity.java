6e7a3844563531a440b4d9413cf6feff741cf769
KIM TAEEUN <snowykte0426@naver.com>
2025-02-18 03:33:54 +0900

    add: Add Base Auto Incr Entity

---
+++ src/main/java/com/gsm/_8th/class4/backed/task/_1/_1/global/entity/BaseIdxEntity.java
@@ -0,0 +1,30 @@
+package com.gsm._8th.class4.backed.task._1._1.global.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+abstract class BaseIdxEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idx", nullable = false, updatable = false, insertable = false, unique = true)
+    protected Long idx;
+    @Column(nullable = false, name = "created_at")
+    protected LocalDateTime createdAt;
+    @Column(nullable = false, name = "updated_at")
+    protected LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onPersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
