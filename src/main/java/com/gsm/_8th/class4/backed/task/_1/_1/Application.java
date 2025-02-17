28e30598c9ea64847420b6c33a1838f171c2139a
KIM TAEEUN <snowykte0426@naver.com>
2025-02-18 03:33:15 +0900

    init: Springboot Project Init

---
+++ src/main/java/com/gsm/_8th/class4/backed/task/_1/_1/Application.java
@@ -0,0 +1,13 @@
+package com.gsm._8th.class4.backed.task._1._1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsm/_8th/class4/backed/task/_1/_1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsm._8th.class4.backed.task._1._1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
