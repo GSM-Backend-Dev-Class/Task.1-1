0692359e1c62a4feb9559b644d4004c165dcf156
ZaMan <roblery128@gmail.com>
2025-07-15 23:06:46 +0900

    Interface DI

--- src/main/java/com/gsm/_8th/class4/backed/task/_1/_1/domain/article/controller/ArticleController.java
+++ src/main/java/com/gsm/_8th/class4/backed/task/_1/_1/domain/article/controller/ArticleController.java
@@ -2,7 +2,7 @@ package com.gsm._8th.class4.backed.task._1._1.domain.article.controller;
 
 import com.gsm._8th.class4.backed.task._1._1.domain.article.dto.ArticleInfo;
 import com.gsm._8th.class4.backed.task._1._1.domain.article.dto.ArticleResponse;
-import com.gsm._8th.class4.backed.task._1._1.domain.article.service.ArticleServiceImpl;
+import com.gsm._8th.class4.backed.task._1._1.domain.article.service.ArticleService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,34 +15,34 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/articles")
 public class ArticleController {
-    private final ArticleServiceImpl articleServiceImpl;
+    private final ArticleService articleService;
 
     @GetMapping
     public ResponseEntity<List<ArticleResponse>> getArticles() {
-        return ResponseEntity.status(HttpStatus.OK).body(articleServiceImpl.getArticles());
+        return ResponseEntity.status(HttpStatus.OK).body(articleService.getArticles());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ArticleResponse> getArticleById(@PathVariable Long id) {
-        ArticleResponse articleResponse = articleServiceImpl.viewArticle(id);
+        ArticleResponse articleResponse = articleService.viewArticle(id);
         return ResponseEntity.status(HttpStatus.OK).body(articleResponse);
     }
 
     @PostMapping
     public ResponseEntity<Void> postArticle(@Valid @RequestBody ArticleInfo articleInfo) {
-        articleServiceImpl.postArticle(articleInfo);
+        articleService.postArticle(articleInfo);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity<Void> editArticle(@PathVariable Long id, @Valid @RequestBody ArticleInfo articleInfo) {
-        articleServiceImpl.editArticle(id, articleInfo);
+        articleService.editArticle(id, articleInfo);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteArticle(@PathVariable Long id) {
-        articleServiceImpl.deleteArticle(id);
+        articleService.deleteArticle(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
